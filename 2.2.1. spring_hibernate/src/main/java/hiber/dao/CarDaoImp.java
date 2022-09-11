a0b57a6f0c2b435ca3e7eb911f41a2f862caf2ec
halitov-r-k <halitov.r.k@gmail.com>
2022-09-11 09:40:50 +0500

    2022-09-11T09:40 В классах UserDaoImp.java, UserServiceImp.java добавлены конструкторы, аннотации @Autowired убраны. Удалены класс CarServiceImp.java и интерфейс CarService.java.

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/CarDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/CarDaoImp.java
@@ -2,15 +2,15 @@ package hiber.dao;
 
 import hiber.model.Car;
 import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.TypedQuery;
 import java.util.List;
 @Repository
 public class CarDaoImp implements CarDao{
-    @Autowired
-    private SessionFactory sessionFactory;
+    private final SessionFactory sessionFactory;
+
+    public CarDaoImp(SessionFactory sessionFactory) { this.sessionFactory = sessionFactory; }
 
     @Override
     public void add(Car car) {
